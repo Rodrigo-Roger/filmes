2c207f627f7785b08e7b93d7fe04b5c7e7ce0ee4
Rodrigo Roger <rodrigoroger002@gmail.com>
2023-04-12 21:39:46 -0300

    ajustando a imagens e algumas informações

--- components/pagina.js
+++ components/pagina.js
@@ -1,26 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import Cabecalho from './cabecalho'
+import Cabecalho from './Cabecalho'
 
 const Pagina = (props) => {
-
-  return (
-    <>
-      <Cabecalho />
-      <div className='bg-secondary py-3 text-white text-right mb-3'>
-        <Container>
-          <h1>{props.titulo}</h1>
-        </Container>
-      </div>
-    <Container className='mb-5'>
-      {props.children}
-      </Container>
-      <div style={{ width: '100%' }} className='bg-secondary position-fixed bottom-0 py-3 text-white text-center'>
-        <p>Todos os direitos reservados</p>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Cabecalho />
+            <div className='bg-secondary py-3 text-white text-center mb-3'>
+                <Container>
+                    <h1>{props.titulo}</h1>
+                </Container>
+            </div>
+            <Container>
+                {props.children}
+            </Container>
+            <div style={{ width: '100%' }} className='bg-secondary py-3 text-white text-center position-fixed bottom-0'>
+                <div>Reservados os direitos®</div>
+            </div>
+        </>
+    )
 }
 
 export default Pagina
--- pages/filmes/index.js
+++ pages/filmes/index.js
@@ -1,23 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import Pagina from '../../components/Pagina'
+import React ,{ useEffect, useState }  from 'react'
+import Pagina from '../../components/pagina'
 import apiFilmes from '../../services/apiFilmes'
+import { Button, Card, Col, Row } from 'react-bootstrap'
 
 const index = () => {
-    const[filmes, setFilmes] = useState([])
 
-    useEffect(()=>{
-        apiFilmes.get('/movie/popular').then(resultado =>{
-            setFilmes(resultado.data.results)
-        })
-    }, [])
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(()=>{
+
+    apiFilmes.get('/movie/popular').then(resultado=>{
+      setFilmes(resultado.data.results)
+    })
+
+  }, [])
+
   return (
-    <Pagina titulo='Filmes'>
+    <Pagina titulo="Página de Filmes">
 
-        {filmes.map(item => (
-            <p>{item.title}</p>
-            
-            
-        ))}
+      <Row md={3}>
+
+      {filmes.map(item => (
+        <Col key={item.id}>
+        <Card className='mb-3'>
+          <Card.Img variant='top' src={'https://image.tmdb.org/t/p/w500'+item.backdrop_path}></Card.Img>
+          <Card.Body>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Text>Lançamento: {item.release_date}</Card.Text>
+            <Card.Text>Nota: {item.vote_average}</Card.Text>
+            <Button variant='primary'>Detalhes</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+      ))}
+      </Row>
+
+
     </Pagina>
   )
 }
