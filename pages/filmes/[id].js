5aa70b477394c7237d2149c56543af3fc98625b0
Rodrigo <rodrigorodgrt002@gmail.com>
2023-04-19 21:25:05 -0300

    :thumbsup:

--- pages/filmes/[id].js
+++ pages/filmes/[id].js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Col } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 import Pagina from '../../components/pagina'
 import apiFilmes from '../../services/apiFilmes'
 
@@ -8,8 +8,23 @@ const Detalhes = ({filme}) => {
     <Pagina titulo={filme.title}>
     
     <Row>
-        <Col >
-        <Card className=''></Card>
+        <Col md={3}>
+        <Card.Img variant='top' src={'https://image.tmdb.org/t/p/w500'+filme.poster_path}></Card.Img>
+        </Col>
+        <Col md={9}>
+          <p><strong>Data de Lançamento: </strong>{filme.release_date}</p>
+          <p><strong>Orçamento: </strong>{filme.budget}</p>
+          <p><strong>Duração: </strong>{filme.runtime}</p>
+          <p><strong>Nota: </strong>{filme.vote_average}</p>
+          <div>
+            <ul>
+              
+              {filme.genres.map(item => (
+            <li>{item.name}</li>
+        ))}
+              
+            </ul>
+          </div>
         </Col>
     </Row>
     </Pagina>
@@ -23,7 +38,7 @@ export async function getServerSideProps(context){
 
     const id = context.params.id
 
-    const resultado=await apiFilmes.get("/movie/"+id)
+    const resultado=await apiFilmes.get("/movie/"+id+'?language=pt-BR')
     const filme = resultado.data
     return{
       props:{filme},
